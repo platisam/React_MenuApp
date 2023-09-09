efe091f050da16f92c8212b29172c7d5e6305fcc
platisam <platisam@gmail.com>
2023-09-09 11:29:43 +0200

    setting state for categories and menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import items from "./data";
+import Categories from "./Categories";
+import Menu from "./Menu";
 
 function App() {
-  return <h2>Menu App</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
